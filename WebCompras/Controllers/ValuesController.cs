c5b8805f204e9b9ec7553a1e0f969ce22bf71b0b
Freddy Remache <fremache3525@uta.edu.ec>
2023-03-11 22:44:29 -0500

    hola

--- WebCompras/Controllers/ValuesController.cs
+++ WebCompras/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace WebCompras.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return "value2";
         }
 
         // POST api/values
